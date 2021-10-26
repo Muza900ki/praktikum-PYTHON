0d1cb99d7a6d14431f32782f343d550f7748c63c
Muzaki Abdullah Irsyad <92155347+Muza900ki@users.noreply.github.com>
2021-10-25 19:04:34 -0800

    Praktikum Mandiri Python 2021

---
+++ Praktek DDP#PYTHON/praktek1.py
@@ -0,0 +1,6 @@
+#Latihan Python
+nama = "Muzaki Abdullah Irsyad"
+jenis_kelamin = 'L'
+umur = "18 Tahun"
+pekerjaan = "Mahasiswa"
+print("Nama :",nama,"\nJenis kelamin :",jenis_kelamin,"\nUmur: ",umur,"\nPekerjaan :",pekerjaan) ;
